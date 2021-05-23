de0643a5726059e8f83a59edb50115d6b987e43a
Natalia Dziedzic <nd.nataliadziedzic@gmail.com>
2021-05-23 12:59:01 +0200

    Style the login box

--- src/components/Login/Login.style.ts
+++ src/components/Login/Login.style.ts
@@ -1,9 +1,62 @@
 import styled from 'styled-components';
+import { TextField } from '@material-ui/core';
+import milkyWay from '../../assets/images/milky-way.png';
 
 export const Box = styled.div`
   width: 400px;
-  height: 500px;
+  height: 600px;
+  overflow: hidden;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 230px 370px;
   margin-top: 100px;
-  background-color: #fff;
+  background-image: url(${milkyWay});
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
   border-radius: 20px;
+  box-shadow: 6px 2px 12px black;
+  border: 3px solid black;
+  h2 {
+    color: #006fb1;
+    text-align: center;
+    font-size: 1.9rem;
+    margin: 16px 0;
+  }
+`;
+export const LoginSection = styled.section`
+  grid-row: 2;
+  border-radius: 50% 0 0 0;
+  background-color: #fff;
+  button {
+    width: 140px;
+    margin: 40px auto 8px;
+    display: block;
+  }
+  .createAccount,
+  .forgotPassword {
+    display: block;
+    text-align: center;
+    font-size: 12px;
+    color: #828282;
+    cursor: pointer;
+    &:hover {
+      color: #000;
+    }
+  }
+  .forgotPassword {
+    width: 250px;
+    margin: 0 auto;
+    text-align: left;
+  }
+`;
+export const StyledTextfield = styled(TextField)`
+  width: 250px;
+  margin: 16px auto;
+  display: block;
+  fieldset {
+    border: none;
+    border-bottom: 1px solid black;
+    border-radius: 0;
+  }
 `;
--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,10 +1,53 @@
+import { Button } from '@material-ui/core';
 import * as React from 'react';
-import { Box } from './Login.style';
+import { Box, LoginSection, StyledTextfield } from './Login.style';
 
 export interface LoginProps {}
 
 const Login: React.FC<LoginProps> = () => {
-  return <Box></Box>;
+  const [user, setUser] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  const login = () => {
+    console.log('LOGIN');
+  };
+
+  return (
+    <Box>
+      <LoginSection>
+        <h2>Login</h2>
+        <StyledTextfield
+          variant='outlined'
+          margin='normal'
+          fullWidth
+          id='email'
+          label='Email/username'
+          name='email'
+          autoComplete='email'
+          autoFocus
+          value={user}
+          onChange={event => setUser(event.target.value)}
+        />
+        <StyledTextfield
+          variant='outlined'
+          margin='normal'
+          fullWidth
+          name='password'
+          label='Password'
+          type='password'
+          id='password'
+          autoComplete='current-password'
+          value={password}
+          onChange={event => setPassword(event.target.value)}
+        />
+        <span className='forgotPassword'>Forgot password?</span>
+        <Button variant='contained' color='primary' onClick={login}>
+          Login
+        </Button>
+        <span className='createAccount'>Create account?</span>
+      </LoginSection>
+    </Box>
+  );
 };
 
 export default Login;
