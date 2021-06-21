5cdc6c04c2e6e4dd0ed8a5c644e303d810dc7d69
Natalia Dziedzic <nd.nataliadziedzic@gmail.com>
2021-06-21 09:06:18 +0200

    Remove unused line of code

--- src/components/Memes/SingleMeme/SingleMeme.style.ts
+++ src/components/Memes/SingleMeme/SingleMeme.style.ts
@@ -1,6 +1,5 @@
 import styled from 'styled-components'
 import { SIZES } from '../../../assets/styles/mediaQueries'
-import { colors } from '../../../assets/styles/theme'
 
 export const Meme = styled.div`
   display: flex;
